6e2012dcc20079531fee8f6682c1db0b51739b8f
k21066-Shimizu <k21066kk@aitech.ac.jp>
2022-12-27 12:49:13 +0900

    地図の表示

--- static/ramen_map.js
+++ static/ramen_map.js
@@ -1,5 +1,9 @@
-const main = () => {
-    console.log("Hello, World!");
+const main = async () => {
+    const map = L.map('map').setView([35.176370507601106, 137.10632646220583], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 };
 
 window.onload = main;
