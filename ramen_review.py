221b780686d48636922a27a3336821ecf1279c49
k21066-Shimizu <k21066kk@aitech.ac.jp>
2022-12-27 13:17:58 +0900

    APIからラーメン屋を検索できるように

--- ramen_review.py
+++ ramen_review.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect
+import requests
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
@@ -6,8 +7,15 @@ app.config["JSON_AS_ASCII"] = False
 PORT = 8080
 
 @app.route('/ramen-map', methods=["GET"])
-def address_get():
+def ramen_map():
     return render_template("ramen_map.html")
+
+@app.route('/api/ramen-shop', methods=["GET"])
+def ramen_shop():
+    # print('?' + '&'.join(f"{v[0]}={','.join(v[1])}" for v in request.args.lists()))
+    url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+    r = requests.get(url, request.args.to_dict())
+    return r.json()
 
 @app.route('/')
 def index():
--- static/ramen_map.js
+++ static/ramen_map.js
@@ -1,9 +1,27 @@
+const API_KEY = "xxx";
+
 const main = async () => {
     const map = L.map('map').setView([35.176370507601106, 137.10632646220583], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    const url = new URL("/api/ramen-shop", window.location);
+    url.search = new URLSearchParams({
+        key: API_KEY,
+        keyword: "ラーメン",
+        genre: "G013",
+        middle_area: "Y200",
+        format: "json",
+    });
+    const response = await fetch(url);
+    if (!response.ok) {
+        console.error(response);
+        return;
+    };
+    const json = await response.json();
+    console.log(json);
 };
 
 window.onload = main;
