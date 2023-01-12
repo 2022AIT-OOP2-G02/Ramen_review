c142f66db78ce1def03713a0f238c221a9077a2e
k21066-Shimizu <k21066kk@aitech.ac.jp>
2023-01-12 14:20:41 +0900

    ピンの最大数を増加 ズームレベルに応じて検索範囲を変えるように

--- static/ramen_map.js
+++ static/ramen_map.js
@@ -1,6 +1,3 @@
-const RANGE_PATTERN = { 1: 300, 2: 500, 3: 1000, 4: 2000, 5: 3000 };
-const RANGE_VALUE = 5;
-
 const DEFAULT_LATLNG = [35.176370507601106, 137.10632646220583];
 
 const main = async () => {
@@ -10,37 +7,47 @@ const main = async () => {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-
     const markers = new Set();
-    map.on('moveend', async () => {
-        const center = map.getCenter();
-        const json = await fetchRamenShop({
-            lat: center.lat,
-            lng: center.lng,
-            range: RANGE_VALUE,
-        });
+    map.on('moveend', displayRamenShop.bind(null, map, markers));
+};
 
-        for (const marker of markers) {
-            map.removeLayer(marker);
-            markers.delete(marker);
-        }
-
-        const circle = L.circle(center, {
-            radius: RANGE_PATTERN[RANGE_VALUE],
-            color: 'blue',
-            fillColor: '#399ade',
-            fillOpacity: 0.5
-        });
-        circle.addTo(map);
-        markers.add(circle);
-
-        for (const shop of json.shop) {
-            const marker = L.marker([shop.lat, shop.lng]);
-            marker.bindPopup(shop.name);
-            marker.addTo(map);
-            markers.add(marker);
-        }
+const displayRamenShop = async (map, markers) => {
+    const center = map.getCenter();
+    const range = getProperRange(map.getZoom());
+    const json = await fetchRamenShop({
+        lat: center.lat,
+        lng: center.lng,
+        range: range.level,
     });
+
+    for (const marker of markers) {
+        map.removeLayer(marker);
+        markers.delete(marker);
+    }
+
+    const circle = L.circle(center, {
+        radius: range.meter,
+        color: 'blue',
+        fillColor: '#399ade',
+        fillOpacity: 0.5
+    });
+    circle.addTo(map);
+    markers.add(circle);
+
+    for (const shop of json.shop) {
+        const marker = L.marker([shop.lat, shop.lng]);
+        marker.bindPopup(shop.name);
+        marker.addTo(map);
+        markers.add(marker);
+    }
+};
+
+const getProperRange = (zoom) => {
+    if (zoom >= 18) return { level: 1, meter: 300 };
+    if (zoom >= 17) return { level: 2, meter: 500 };
+    if (zoom >= 16) return { level: 3, meter: 1000 };
+    if (zoom >= 15) return { level: 4, meter: 2000 };
+    return { level: 5, meter: 3000 };
 };
 
 const fetchRamenShop = async (serchParam) => {
@@ -49,6 +56,7 @@ const fetchRamenShop = async (serchParam) => {
         ...serchParam,
         genre: "G013",
         format: "json",
+        count: 100
     });
     const response = await fetch(url);
     if (!response.ok) {
